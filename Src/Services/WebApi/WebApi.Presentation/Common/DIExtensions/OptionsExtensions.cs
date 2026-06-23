35ff1e804bd4e2e9f70e0846fae5946f51d4fd31
MatinDeWet <matindewet116@gmail.com>
2026-06-23 18:02:34 +0200

    refactor(di): drop xml comments from OptionsExtensions

--- Src/Services/WebApi/WebApi.Presentation/Common/DIExtensions/OptionsExtensions.cs
+++ Src/Services/WebApi/WebApi.Presentation/Common/DIExtensions/OptionsExtensions.cs
@@ -4,9 +4,6 @@ namespace WebApi.Presentation.Common.DIExtensions;
 
 public static class OptionsExtensions
 {
-    /// <summary>
-    /// Binds the application's strongly-typed configuration sections to the options pattern.
-    /// </summary>
     public static IServiceCollection AddApplicationOptions(this IServiceCollection services, IConfiguration configuration)
     {
         services.Configure<InvitationOptions>(configuration.GetSection(InvitationOptions.SectionName));
--- Src/Services/Worker/Worker.Presentation/Common/DIExtensions/OptionsExtensions.cs
+++ Src/Services/Worker/Worker.Presentation/Common/DIExtensions/OptionsExtensions.cs
@@ -4,9 +4,6 @@ namespace Worker.Presentation.Common.DIExtensions;
 
 public static class OptionsExtensions
 {
-    /// <summary>
-    /// Binds the worker's strongly-typed configuration sections to the options pattern.
-    /// </summary>
     public static IServiceCollection AddWorkerOptions(this IServiceCollection services, IConfiguration configuration)
     {
         services.Configure<InvitationPurgeOptions>(configuration.GetSection(InvitationPurgeOptions.SectionName));
